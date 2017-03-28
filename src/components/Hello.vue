f4023c3f3980d3ff28969678c6375717580856e0
zjq41981 <zjq41981@ly.com>
2017-03-28 09:30:35 +0800

    练习1

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -2,21 +2,10 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div v-html="innerHtml"></div>
+    <span v-once>what the fuck?!!{{msg}}</span>
+    <button @click="change">123</button>
+    <p v-if="seen">do u miss me?</p>
   </div>
 </template>
 
@@ -25,7 +14,16 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      seen:true,
+      msg: 'hehedale',
+      innerHtml:'<button>html</button>'
+    }
+  },
+  methods:{
+    change:function () {
+      var _this = this;
+      _this.msg='kl';
+      console.log(123);
     }
   }
 }
